28526cb1e49cf4f34f7a28cd0f843e4ad84ff7a6
sunayanaseshadri <sunayana.seshadri@gmail.com>
2026-02-02 19:54:28 -0500

    Update 06_shipping_analysis.sql

--- sql_queries/06_shipping_analysis.sql
+++ sql_queries/06_shipping_analysis.sql
@@ -1,1 +1,10 @@
+SELECT 
+    p.category,
+    SUM(oi.sales) AS revenue,
+    SUM(oi.profit) AS profit,
+    ROUND(SUM(oi.profit)/SUM(oi.sales)*100, 2) AS profit_margin_pct
+FROM products p
+JOIN order_items oi ON p.product_id = oi.product_id
+GROUP BY p.category
+ORDER BY profit DESC;
 
