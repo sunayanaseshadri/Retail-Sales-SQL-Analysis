c1d6b6442faf17efe81c03430f17866ac0f92d6b
sunayanaseshadri <sunayana.seshadri@gmail.com>
2026-02-02 19:54:50 -0500

    Update 04_customer_analysis.sql

--- sql_queries/04_customer_analysis.sql
+++ sql_queries/04_customer_analysis.sql
@@ -1,1 +1,12 @@
+SELECT 
+    c.customer_id,
+    c.name,
+    COUNT(DISTINCT o.order_id) AS total_orders,
+    SUM(oi.sales) AS total_spend,
+    ROUND(AVG(oi.sales), 2) AS avg_order_value
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+GROUP BY c.customer_id, c.name
+ORDER BY total_spend DESC;
 
