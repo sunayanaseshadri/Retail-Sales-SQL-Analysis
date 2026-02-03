7c5fffeaee7bba1a3ed12d626ba574bcd702e5e4
sunayanaseshadri <sunayana.seshadri@gmail.com>
2026-02-02 19:55:03 -0500

    Update 03_product_performance.sql

--- sql_queries/03_product_performance.sql
+++ sql_queries/03_product_performance.sql
@@ -1,1 +1,11 @@
+SELECT 
+    p.product_name,
+    p.category,
+    SUM(oi.sales) AS revenue,
+    SUM(oi.profit) AS profit,
+    ROUND(SUM(oi.profit)/SUM(oi.sales)*100, 2) AS profit_margin_pct
+FROM products p
+JOIN order_items oi ON p.product_id = oi.product_id
+GROUP BY p.product_name, p.category
+ORDER BY revenue DESC;
 
