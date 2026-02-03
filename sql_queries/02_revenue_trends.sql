48668c4468174b86ec848f3331b5ae02622753fd
sunayanaseshadri <sunayana.seshadri@gmail.com>
2026-02-02 19:55:13 -0500

    Update 02_revenue_trends.sql

--- sql_queries/02_revenue_trends.sql
+++ sql_queries/02_revenue_trends.sql
@@ -1,1 +1,9 @@
+SELECT 
+    DATE_FORMAT(order_date, '%Y-%m') AS month,
+    SUM(oi.sales) AS monthly_revenue,
+    SUM(oi.profit) AS monthly_profit
+FROM orders o
+JOIN order_items oi ON o.order_id = oi.order_id
+GROUP BY month
+ORDER BY month;
 
