dfa15423b137d7df69c2a92cec821f008afdc58f
sunayanaseshadri <sunayana.seshadri@gmail.com>
2026-02-02 19:55:23 -0500

    Update 01_data_overview.sql

--- sql_queries/01_data_overview.sql
+++ sql_queries/01_data_overview.sql
@@ -1,1 +1,5 @@
+SELECT COUNT(*) AS total_customers FROM customers;
+SELECT COUNT(*) AS total_products FROM products;
+SELECT COUNT(*) AS total_orders FROM orders;
+SELECT COUNT(*) AS total_order_items FROM order_items;
 
